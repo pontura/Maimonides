864420a32adb496d7a0e62e7835de36d42fc38a8
Agustin Pontura <pontura@gmail.com>
2018-05-24 11:21:05 -0300

    clock api

---
+++ introduccion/Assets/Clock.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clock : MonoBehaviour {
+
+	public float speed = 1;
+	float tick;
+
+	public int sec;
+	public int min;
+	public int hours;
+
+	void Start () {
+		sec = System.DateTime.Now.Second;
+		min = System.DateTime.Now.Minute;
+		hours = System.DateTime.Now.Hour;
+	}
+
+	void Update () {
+		tick +=  Time.deltaTime * speed;
+		if (tick > 1) {
+			sec+=(int)Mathf.Round(tick);
+			tick = 0;
+		}
+		if (sec >= 60) {
+			sec = 0;
+			min++;
+		}
+		if (min >= 60) {
+			min = 0;
+			hours++;
+		}
+		if (hours >= 24) {
+			hours = 0;
+		}
+	}
+}
---
+++ introduccion/Assets/ClockUI.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClockUI : MonoBehaviour {
+
+	public Text field;
+	public Clock clock;
+	public Slider sliderSpeed;
+	public Slider sliderHour;
+
+	void Update () {
+		ShowTime();
+	}
+	public void ChangeSpeed()
+	{
+		clock.speed = sliderSpeed.value;
+	}
+	public void ChangeHour()
+	{
+		clock.hours = (int)sliderHour.value;
+	}
+	void ShowTime()
+	{
+		field.text = AddZero(clock.hours) + ":" + AddZero(clock.min) + ":" + AddZero(clock.sec);
+	}
+	string AddZero(int value)
+	{		
+		string str = value.ToString ();
+		if (value < 10) {
+			str = "0" + str;
+		}
+		return str;
+	}
+}
